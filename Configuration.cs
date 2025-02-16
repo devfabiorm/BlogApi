0a4ffca3552d992fac1c94c1a02c8d9078a4e93e
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-02-15 23:43:51 -0300

    wip: Creating service to generate and handle jwt tokens

---
+++ Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace BlogApi;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "QDjMNFhqaz8Ld35GVNb0ygNmHQLhD0Ipsuj7";
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using BlogApi.Models;
+using BlogApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpPost("v1/login")]
+    public IActionResult Login([FromServices] TokenService tokenService)
+    {
+        var token = tokenService.GenerateToken(new User());
+
+        return Ok(token);
+    }
+}
+
