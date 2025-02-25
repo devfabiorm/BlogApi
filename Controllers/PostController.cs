f9abc7904e96adf7a851e88184b4ae1cdf5ab236
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-02-24 21:54:12 -0300

    feat: filtering data

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -51,4 +51,75 @@ public class PostController : ControllerBase
             return StatusCode(500, new ResultViewModel<Post>("05x04 - Falha interna no Servidor"));
         }
     }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context,
+        [FromRoute] int id)
+    {
+        try
+        {
+            var post = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .ThenInclude(x => x.Roles)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync();
+
+            if (post == null)
+                return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<Post>(post));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Post>("05x04 - Falha interna no Servidor"));
+        }
+    }
+
+
+    [HttpGet("v1/posts/category/{category}")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context,
+        [FromRoute] string category,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var count = await context.Posts.AsNoTracking().CountAsync();
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Where(x => x.Category.Slug == category)
+                .Select(x => new ListPostViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Post>("05x04 - Falha interna no Servidor"));
+        }
+    }
 }
